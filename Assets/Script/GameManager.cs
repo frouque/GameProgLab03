20d6e6f5c70d5f7d2399b7105ed59abdb6ca4801
frouque <54182792+frouque@users.noreply.github.com>
2023-09-07 09:27:17 -0400

    assets folder

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+    // Write down your variables here
+    int score = 0;
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void IncrementScore()
+    {
+        ++score;
+        if(score >= 4)
+        {
+            Debug.Log("You Win !");
+        }
+    }
+}
